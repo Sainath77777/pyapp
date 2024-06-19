8df44c66c730000e70bb0b51bdce6bb72dd635f3
Sainath Reddy <112744586+Sainath77777@users.noreply.github.com>
2024-06-19 12:18:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome to python app!"
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0",port=5000)
